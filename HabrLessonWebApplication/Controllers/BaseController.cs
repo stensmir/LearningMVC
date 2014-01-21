90a146614383e487c4800ffc180fc161cf786ece
Viacheslav <bobik.slava@live.com>
2014-01-21 17:21:13 +0400

    add base controller

---
+++ HabrLessonWebApplication/Controllers/BaseController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HabrLessonClassLibrary;
+
+namespace HabrLessonWebApplication.Controllers
+{
+    public class BaseController : Controller
+    {
+        //
+        // GET: /Base/
+        public Domain.User CurrentUser { get; set; }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
