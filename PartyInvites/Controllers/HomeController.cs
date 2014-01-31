8c9b1fe5d0033e3ade1d8e3a7a215cda775d4ec5
Viacheslav <bobik.slava@live.com>
2014-01-31 18:03:55 +0400

    add sms send functionality

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PartyInvites.Models;
+using System.Net.Http;
 
 namespace PartyInvites.Controllers
 {
@@ -30,6 +31,10 @@ namespace PartyInvites.Controllers
             if (ModelState.IsValid)
             {
                 //TODO: Email response to the party organizer
+                using (var client = new HttpClient())
+                {
+                    var response = client.GetStringAsync(string.Format("http://sms.ru/sms/send?api_id=274a8727-80d9-1514-5d69-95ad1fbd45d4&to={0}&text=Хорошей+вечеринки+{1}&from=BobInc", guestResponse.Phone, guestResponse.Name)).Result;
+                }
                 return View("Thanks", guestResponse);
             }
             else
