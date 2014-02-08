114e3187fd76b541239a46aab3b14502d21634cf
Slava Bobik <bobik.slava@live.com>
2014-02-08 19:49:48 +0400

    upd

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
@@ -17,9 +18,16 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ViewResult RpsvForm(GuestRespone guestResponse)
+        {
+            return View("Thanks", guestResponse);
         }
 	}
 }
