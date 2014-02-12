e9221be4a440c56f8f67eb2fec28e76a4261d6c1
Viacheslav <bobik.slava@live.com>
2014-02-12 18:01:23 +0400

    work with styles

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,20 +1,14 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PartyInvites.Models;
-using Moq'
+using Moq;
 
 namespace Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        private Product[] products = 
-        {
-            new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
-            new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
-            new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
-            new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
-        };
+        
         [TestMethod]
         public void TestMethod1()
         {
